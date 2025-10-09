86d4913e74c6c2f615126d1d92335815e4235777
Erick Huayllani <74062867@continental.edu.pe>
2025-10-08 20:35:11 -0500

    Primera versión del sitio Repostería Rossy

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
-  theme: { extend: {} },
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
